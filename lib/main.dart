b6d6531612c0f8a9ec075aa8e5e231fe3e561f67
Việt Hùng <viet.hung.2898@gmail.com>
2022-01-02 12:29:18 +0700

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,14 +32,90 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class QuoteList extends StatefulWidget {
+  const QuoteList({ Key? key }) : super(key: key);
+
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<Quote> quotes = [
+    Quote(author: 'Rowdy', text: 'No thing'),
+    Quote(author: 'Rowdy', text: 'No thing 2'),
+    Quote(author: 'Rowdy', text: 'No thing 3'),
+    Quote(author: 'Rowdy', text: 'No thing 4'),
+    Quote(author: 'Rowdy', text: 'No thing 5')
+  ];
+
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16, 16, 16, 16),
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              '${quote.text}',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey
+              ),
+            ),
+            SizedBox(height: 6,),
+            Text(
+              '${quote.author}',
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.grey
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Divider(
+          height: 60,
+          color: Colors.grey[800],
+        ),
+        Text(
+          'Quote',
+          style: TextStyle(
+            color: Colors.grey,
+            letterSpacing: 2,
+            fontSize: 20,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+        SizedBox(height: 10,),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+        )
+      ]
+    );
+  }
+}
+
 class RowdyCart extends StatefulWidget {
-  const RowdyCart({ Key? key }) : super(key: key);
+  const RowdyCart({Key? key}) : super(key: key);
 
   @override
   State<RowdyCart> createState() => _RowdyCartState();
 }
 
 class _RowdyCartState extends State<RowdyCart> {
+  int rowdyLevel = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -65,40 +142,40 @@ class _RowdyCartState extends State<RowdyCart> {
             ),
             Text(
               'Name',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0
-              ),
+              style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
             ),
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
             Text(
-              'Viet Hung',
+              'Việt Hùng',
               style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2,
-                fontSize: 28,
-                fontWeight: FontWeight.bold
-              ),
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 30,),
+            SizedBox(
+              height: 30,
+            ),
             Text(
               'Current rowdy level',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0
-              ),
+              style: TextStyle(color: Colors.grey, letterSpacing: 2.0),
             ),
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
             Text(
-              '8',
+              '$rowdyLevel',
               style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 2,
-                fontSize: 28,
-                fontWeight: FontWeight.bold
-              ),
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 30,),
+            SizedBox(
+              height: 30,
+            ),
             Row(
               children: [
                 Icon(
@@ -115,9 +192,19 @@ class _RowdyCartState extends State<RowdyCart> {
                   ),
                 )
               ],
-            )
+            ),
+            QuoteList()
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            rowdyLevel++;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
       ),
     );
   }
